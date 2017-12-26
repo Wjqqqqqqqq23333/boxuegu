8f52369dc376da7f5d7c49b6a8ef685f3730b1f7
Wjqqqqqqqq23333 <245403699@qq.com>
2017-12-26 15:09:09 +0800

    温均钦

--- app/src/main/java/android/gdmec/edu/cn/boxuegu/MainActivity.java
+++ app/src/main/java/android/gdmec/edu/cn/boxuegu/MainActivity.java
@@ -1,14 +1,18 @@
 package android.gdmec.edu.cn.boxuegu;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView tv_back;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tv_exercises;
     private TextView tv_myinfo;
     private ImageView iv_course;
+    private ImageView iv_exercises;
+    private ImageView iv_myinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         init();
         initBottomBar();
         setListener();
+        setInitStatus();
 
     }
+
+    private void setInitStatus() {
+        clearBottomImageState();
+        setSelectedStatus(0);
+        createView(0);
+    }
+
     private void setListener(){
         for(int i=0;i<mBottomLayout.getChildCount();i++){
-            mBodyLayout.getChildAt(i).setOnClickListener(this);
+            mBottomLayout.getChildAt(i).setOnClickListener(this);
         }
     }
     private void initBottomBar(){
@@ -49,6 +63,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_myinfo = (TextView) findViewById(R.id.bottom_bar_text_myinfo);
 
         iv_course = (ImageView)findViewById(R.id.bottom_bar_image_course);
+        iv_exercises = (ImageView)findViewById(R.id.bottom_bar_image_exercises);
+        iv_myinfo = (ImageView)findViewById(R.id.bottom_bar_image_myinfo);
     }
     private void init(){
         tv_back = ((TextView) findViewById(R.id.tv_back));
@@ -88,7 +104,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void selectDisplayView(int i) {
+    /**
+     * 显示对应页面
+     * @param index
+     */
+    private void selectDisplayView(int index) {
+        removeAllView();
+        createView(index);
+        setSelectedStatus(index);
+    }
+
+    /**
+     * 设置底部按钮的选中状态
+     * @param index
+     */
+    private void setSelectedStatus(int index) {
+        switch (index){
+            case 0:
+                mCourseBtn.setSelected(true);
+                iv_course.setImageResource(R.drawable.main_course_icon_selected);
+                tv_course.setTextColor(Color.parseColor("#0097f7"));
+                rl_title_bar.setVisibility(View.VISIBLE);
+                tv_main_title.setText("博学谷课程");
+                break;
+            case 1:
+                mExerisesBtn.setSelected(true);
+                iv_exercises.setImageResource(R.drawable.main_exercises_icon_selected);
+                tv_exercises.setTextColor(Color.parseColor("#0097f7"));
+                rl_title_bar.setVisibility(View.VISIBLE);
+                tv_main_title.setText("博学谷习题");
+                break;
+            case 2:
+                mMyInfoBtn.setSelected(true);
+                iv_myinfo.setImageResource(R.drawable.main_my_icon_selected);
+                tv_myinfo.setTextColor(Color.parseColor("#0097f7"));
+                rl_title_bar.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
+    private void createView(int ViewIndex) {
+        switch (ViewIndex){
+            case 0:
+                //TODO：课程界面
+                break;
+            case 1:
+                //习题界面
+                break;
+            case 2:
+                //我的界面
+                break;
+        }
+    }
+
+    /**移除不需要的视图
+     *
+     */
+    private void removeAllView() {
+        for (int i = 0;i<mBodyLayout.getChildCount();i++){
+            mBodyLayout.getChildAt(i).setVisibility(View.GONE);
+        }
     }
 
     private void clearBottomImageState() {
@@ -96,7 +171,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_exercises.setTextColor(Color.parseColor("#666666"));
         tv_myinfo.setTextColor(Color.parseColor("#666666"));
 
-        iv_course
+        iv_course.setImageResource(R.drawable.main_course_icon);
+        iv_exercises.setImageResource(R.drawable.main_exercises_icon);
+        iv_myinfo.setImageResource(R.drawable.main_my_icon);
+
+        for(int i=0;i<mBottomLayout.getChildCount();i++){
+            mBottomLayout.getChildAt(i).setSelected(false);
+        }
+    }
+
+    protected  long exitTime;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK &&event.getAction() == KeyEvent.ACTION_DOWN){
+            if((System.currentTimeMillis() - exitTime) > 2000){
+                Toast.makeText(this,  "再按一次退出博学谷", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            }else{
+                MainActivity.this.finish();
+                if(readLoginStatus()){
+                    //如果是已登录用户，需要清除登录状态,清除登录时的用户名
+                clearLoginStatus();
+                }
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+
+    private void clearLoginStatus() {
+        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isLogin",false);
+        editor.putString("loginUserName","");
+        editor.commit();
+    }
+
+    private boolean readLoginStatus() {
+        SharedPreferences sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE);
+        boolean isLogin = sp.getBoolean("isLogin" ,false);
+        return isLogin;
     }
 }
 //111
