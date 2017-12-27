7183e852ecc54f8aee40550ce1021c0a31905d6b
dz6850 <971401108@qq.com>
2017-12-27 10:16:46 +0800

    bean

---
+++ app/src/main/java/android/gdmec/edu/cn/boxuegu/activity/UserInfoActivity.java
@@ -0,0 +1,14 @@
+package android.gdmec.edu.cn.boxuegu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.gdmec.edu.cn.boxuegu.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
