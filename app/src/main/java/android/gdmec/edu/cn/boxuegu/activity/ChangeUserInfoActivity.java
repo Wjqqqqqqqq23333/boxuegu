9338e344d3f280af5dfa9aebc1e568886c7b9720
dz6850 <971401108@qq.com>
2017-12-27 15:21:43 +0800

    修改个人资料的布局

---
+++ app/src/main/java/android/gdmec/edu/cn/boxuegu/activity/ChangeUserInfoActivity.java
@@ -0,0 +1,14 @@
+package android.gdmec.edu.cn.boxuegu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.gdmec.edu.cn.boxuegu.R;
+
+public class ChangeUserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_user_info);
+    }
+}
